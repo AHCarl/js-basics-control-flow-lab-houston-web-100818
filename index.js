cfa754548409d206b0e7c236a16ad556aade58f8
Alexander Carlin <ahcarlin@gmail.com>
2018-11-16 05:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function ternaryCheckCity(city) {
   return (city === "NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmForTip() {
- 
+function switchOnCharmForTip(tip) {
+ case(tip)
  
  default:  
 }
