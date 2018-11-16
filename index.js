81cfb05a6aec4cff347907cc0edf041c6f15a43c
Alexander Carlin <ahcarlin@gmail.com>
2018-11-16 05:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function scuberGreetingForFeet(feet)  {
   let result;
   if (feet <= 401) {
     result = "This one is on me!"
-  } else {
-    result = 
+  } else if (feet >= 2000){
+    result = "I will gladly take your 30 bucks"
   }
     
 }
